4a231d48de6abb20edc20f63d787ca430a68b4c2
bohuntob <tobias.bohun@hs-furtwangen.de>
2022-11-24 13:01:51 +0100

    Commit 1

--- src/main/java/de/hfu/Main.java
+++ src/main/java/de/hfu/Main.java
@@ -1,10 +1,16 @@
 package de.hfu;
 import java.util.Scanner;
+
+/**
+* Einlesen von String. AUsgabe in Uppercase.
+*/
+
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String input=scanner.next();
         System.out.println(input.toUpperCase());
+        scanner.close();
     }
 }
 
